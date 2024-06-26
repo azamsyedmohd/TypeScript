6bcdc0e3038a9b5e010b26f7a761decf0bff0239
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-26 23:52:47 +0530

    Compiled Variable.ts file

--- src/TypeScript/Types/Variables.js
+++ src/TypeScript/Types/Variables.js
@@ -7,3 +7,25 @@ numCatLives += 2.4;
 var gameOver = true;
 gameOver = false;
 // gameOver = "true"; // Type 'string' is not assignable to type 'boolean'.
+var oneVariable = null;
+var secondVariable = undefined;
+// Type Inference
+var value = "Sunil Gavaskar";
+value = "Sachin Tendulkar";
+value = "Virat kohli";
+// value = true; //  Type 'boolean' is not assignable to type 'string'.
+// Any type --> stops checking and type inference.
+var thing = "Hello";
+thing = 1;
+thing = true;
+thing();
+thing.toUpperCase();
+// Usage of Type Annotation
+var movies = ["Mughal-e-Azam", "Sholay", "Zanjeer", "Deewar"];
+var foundMovie;
+for (var _i = 0, movies_1 = movies; _i < movies_1.length; _i++) {
+    var movie = movies_1[_i];
+    if (movie === "Mughal-e-Azam") {
+        foundMovie = movie;
+    }
+}
