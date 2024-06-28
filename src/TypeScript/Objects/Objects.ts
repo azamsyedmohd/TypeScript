1c2428b1523dcda05c3758730e71d825bf5063af
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-28 22:52:33 +0530

    Objects basics continue

--- src/TypeScript/Objects/Objects.ts
+++ src/TypeScript/Objects/Objects.ts
@@ -2,3 +2,12 @@ function printName(person: { first: string; last: string }): void {
   console.log(`${person?.first} ${person?.last}`);
 }
 printName({ first: "Syed", last: "Mohd Azam" });
+
+let coordinate: { x: number; y: number } = {
+  x: 34,
+  y: 4,
+};
+
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
