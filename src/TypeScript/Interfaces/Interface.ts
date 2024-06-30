e7b8584b364e9e82f66f9df86b7d5a4cf5db0667
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-30 12:07:55 +0530

    Great Example added to the Interface

--- src/TypeScript/Interfaces/Interface.ts
+++ src/TypeScript/Interfaces/Interface.ts
@@ -13,3 +13,40 @@ interface PointTwo {
 const numTwo: PointTwo = { x: Math.random() * 2, y: Math.random() * 3 };
 console.log(numOne);
 console.log(numTwo);
+
+type another = {
+  b: number;
+};
+
+type obj = {
+  a: string;
+  y: another;
+};
+
+type Point = {
+  x: number;
+  y: number;
+  z: string;
+  v: string[];
+  w: obj;
+  p: null;
+  q: undefined;
+  r: boolean;
+};
+
+const pointExample: Point = {
+  x: 34,
+  y: 56,
+  z: "Syed Mohd Azam",
+  v: ["Syed", "Muhammad", "Azam"],
+  p: null,
+  q: undefined,
+  r: true,
+  w: {
+    a: "Good Job",
+    y: {
+      b: 54,
+    },
+  },
+};
+console.log(pointExample);
