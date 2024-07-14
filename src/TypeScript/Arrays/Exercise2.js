a17b31b935431ef7454856506e985732a12aebb3
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-07-14 19:45:05 +0530

    Exercise 2 on Arrays

---
+++ src/TypeScript/Arrays/Exercise2.js
@@ -0,0 +1,3 @@
+// Create an array variable called gameBoard that starts as an empty array.It should be typed to hold a 2 dimensional array of strings.
+var gameBoard = [];
+console.log(gameBoard);
---
+++ src/TypeScript/Arrays/Exercise2.ts
@@ -0,0 +1,4 @@
+// Create an array variable called gameBoard that starts as an empty array.It should be typed to hold a 2 dimensional array of strings.
+
+const gameBoard: string[][] = [];
+console.log(gameBoard);
